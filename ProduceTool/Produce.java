0c1b8d494f5cb7e45f27f94a512209fa3e56b092
NateTRead <64865653+NateTRead@users.noreply.github.com>
2020-06-06 23:20:20 -0700

    Added Produce.java
    Produce.java is a class that can be used to create produce objects for display within a ListView via a ListAdapter.
    
    (Tool to convert raw text into produce objects coming soon)

---
+++ ProduceTool/Produce.java
@@ -0,0 +1,70 @@
+
+
+import java.io.Serializable;
+
+public class Produce implements Serializable {
+    private String name;
+    private String descriptionShort;
+    private String descriptionLong;
+    private String imgURL;
+    private Double seasonRange;
+
+    public Produce(String name, String descriptionLong, String descriptionShort, String imgURL, Double seasonRange){
+        this.name = name;
+        this.descriptionLong = descriptionLong;
+        this.descriptionShort = descriptionShort;
+        this.imgURL = imgURL;
+        this.seasonRange = seasonRange;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescriptionShort() {
+        return descriptionShort;
+    }
+
+    public void setDescriptionShort(String descriptionShort) {
+        this.descriptionShort = descriptionShort;
+    }
+
+    public String getDescriptionLong() {
+        return descriptionLong;
+    }
+
+    public void setDescriptionLong(String descriptionLong) {
+        this.descriptionLong = descriptionLong;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public Double getSeasonRange() {
+        return seasonRange;
+    }
+
+    public void setSeasonRange(Double seasonRange) {
+        this.seasonRange = seasonRange;
+    }
+
+    //TODO format this so it will be more useful : ^ )
+    public String toString() {
+        return "Produce{" +
+                "name='" + name + '\'' +
+                ", descriptionShort='" + descriptionShort + '\'' +
+                ", descriptionLong='" + descriptionLong + '\'' +
+                ", imgURL='" + imgURL + '\'' +
+                ", seasonRange=" + seasonRange +
+                '}';
+    }
+}
